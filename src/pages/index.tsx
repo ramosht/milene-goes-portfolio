02864eb53f2c440a6dc352683156edf1f88e8617
Guilherme <guilhermeht.ramos@gmail.com>
2020-12-08 09:41:57 -0300

    feat: setting up prettier with eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
 
 type Props = {
-  title: string
-}
+  title: string;
+};
 
-export default function Home({title = 'React Avançado!'}: Props) {
+export default function Home({ title = 'React Avançado!' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -14,9 +14,7 @@ export default function Home({title = 'React Avançado!'}: Props) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          {title}
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -65,5 +63,5 @@ export default function Home({title = 'React Avançado!'}: Props) {
         </a>
       </footer>
     </div>
-  )
+  );
 }
